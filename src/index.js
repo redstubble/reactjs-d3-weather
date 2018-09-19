fb8db9a9b201f7c71f10f022fd8c3e25fd686aec
John Williams <john@hothouse.co.nz>
2018-09-19 18:16:48 +1200

    Clean up syntax

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ import * as d3Shape from 'd3-shape';
 import * as d3Axis from 'd3-axis';
 import weatherFetch from './api/weatherApi';
 import testData from './test/testData';
-import { canvas } from './utils/d3-helpers';
+import { canvas as canvasProps } from './utils/d3-helpers';
 import RadioButtons from './utils/radio-buttons';
 
 const graphDiv = '.graph-canvas';
@@ -53,14 +53,14 @@ class D3Weather extends Component {
   };
 
   componentDidUpdate = () => {
-    if (this.state.select === 'weather' && !this.state.d3populated) {
+    const { select, d3populated } = this.state;
+    if (select === 'weather' && !d3populated) {
       this.setState({
         d3populated: true,
       });
       this.populateWeather();
     }
-    if (this.state.select === 'clear' && this.state.d3populated) {
-      debugger;
+    if (select === 'clear' && d3populated) {
       this.setState({
         d3populated: false,
       });
@@ -77,63 +77,42 @@ class D3Weather extends Component {
     }
     const weatherData = await weatherFetch();
     const weatherResults = await Promise.all(weatherData.apiResults.results);
-    var _extends =
-      Object.assign ||
-      function(target) {
-        for (var i = 1; i < arguments.length; i++) {
-          var source = arguments[i];
-          for (var key in source) {
-            if (Object.prototype.hasOwnProperty.call(source, key)) {
-              target[key] = source[key];
-            }
-          }
-        }
-        return target;
-      };
-
-    return _extends({}, weatherData, {
-      apiResults: _extends({}, weatherData.apiResults, {
+    return {
+      ...weatherData,
+      apiResults: {
+        ...weatherData.apiResults,
         results: weatherResults,
-      }),
-    });
-
-    // const result = {
-    //   ...weatherData,
-    //   apiResults: {
-    //     ...weatherData.apiResults,
-    //     results: weatherResults,
-    //   },
-    // };
+      },
+    };
   };
 
   scaffoldCanvas = () => {
-    var containerDiv = document.getElementById('graph-canvas-weather');
-    var svg = d3Selection
+    const containerDiv = document.getElementById('graph-canvas-weather');
+    const { weatherData } = this.state;
+    const svg = d3Selection
       .select(graphDiv)
       .append('svg')
       .attr('height', containerDiv.clientHeight)
       .attr('width', containerDiv.clientWidth);
     const x =
-      containerDiv.clientWidth - (canvas.margin.left + canvas.margin.right);
+      containerDiv.clientWidth -
+      (canvasProps.margin.left + canvasProps.margin.right);
     const y =
-      containerDiv.clientHeight - (canvas.margin.top + canvas.margin.bottom);
+      containerDiv.clientHeight -
+      (canvasProps.margin.top + canvasProps.margin.bottom);
     const node = svg
       .append('g')
       .attr(
         'transform',
-        'translate(' + canvas.margin.left + ',' + canvas.margin.top + ')',
+        `translate(${canvasProps.margin.left},${canvasProps.margin.top})`,
       );
     const dataset = d3Collection
       .nest()
-      .key(function(d) {
-        return d.name;
-      })
-      .rollup(function(d) {
-        return d[0];
-      })
-      .entries(this.state.weatherData.apiResults.results);
+      .key((d) => d.name)
+      .rollup((d) => d[0])
+      .entries(weatherData.apiResults.results);
     return {
-      ...canvas,
+      ...canvasProps,
       ...{
         x,
         y,
@@ -144,73 +123,65 @@ class D3Weather extends Component {
   };
 
   populateGraph = () => {
-    var timeParse = d3TimeFormat.timeParse('%Y-%m-%dT%H:%M');
-    var formatTime = d3TimeFormat.timeFormat('%Y-%m-%dT%H:%M');
-    var arr = this.state.canvas.dataset[0].value.forecast;
-    var range = [
-      d3Array.extent(this.state.canvas.dataset[0].value.forecast, function(d) {
-        return new Date(d.dateTime * 1000);
-      }),
+    const { canvas, weatherData } = this.state;
+    // const timeParse = d3TimeFormat.timeParse('%Y-%m-%dT%H:%M');
+    // const formatTime = d3TimeFormat.timeFormat('%Y-%m-%dT%H:%M');
+    // const arr = this.state.canvas.dataset[0].value.forecast;
+    const range = [
+      d3Array.extent(
+        canvas.dataset[0].value.forecast,
+        (d) => new Date(d.dateTime * 1000),
+      ),
     ];
 
-    var x = d3Scale
+    const x = d3Scale
       .scaleTime()
-      .range([0, this.state.canvas.x])
+      .range([0, canvas.x])
       .domain(range[0]);
 
-    var tempVariance = d3Collection
+    const tempVariance = d3Collection
       .nest()
-      .key(function(d) {
-        return d3Array.max(d.value.forecast, function(d) {
-          return d.temp;
-        });
-      })
-      .map(this.state.canvas.dataset)
+      .key((d) => d3Array.max(d.value.forecast, (e) => e.temp))
+      .map(canvas.dataset)
       .keys();
 
-    var highestTemp = d3Array.max(tempVariance);
-    var lowestTemp = d3Array.min(tempVariance);
+    const highestTemp = d3Array.max(tempVariance);
+    const lowestTemp = d3Array.min(tempVariance);
 
-    var y = d3Scale
+    const y = d3Scale
       .scaleLinear()
-      .range([this.state.canvas.y, 0])
+      .range([canvas.y, 0])
       .domain([lowestTemp - 50, highestTemp]);
 
-    var z = d3Scale.scaleOrdinal(d3ScaleChromatic.schemeCategory10).domain(
-      this.state.weatherData.apiResults.results.map(function(d) {
-        return d.name;
-      }),
-    );
+    const z = d3Scale
+      .scaleOrdinal(d3ScaleChromatic.schemeCategory10)
+      .domain(weatherData.apiResults.results.map((d) => d.name));
 
-    var line = d3Shape
+    const line = d3Shape
       .line()
       // .interpolate('basis')
       .curve(d3Shape.curveNatural)
       //   .curve(d3.curveStepAfter)
-      .x(function(d) {
-        return x(new Date(d.dateTime * 1000));
-      })
-      .y(function(d) {
-        return y(d.temp);
-      });
+      .x((d) => x(new Date(d.dateTime * 1000)))
+      .y((d) => y(d.temp));
 
-    var area = d3Shape
-      .area()
-      .curve(d3Shape.curveStepAfter)
-      .x(function(d) {
-        return x(new Date(d.dateTime * 1000));
-      })
-      .y0(this.state.canvas.x)
-      .y1(function(d) {
-        return y(d.temp);
-      });
+    // const area = d3Shape
+    //   .area()
+    //   .curve(d3Shape.curveStepAfter)
+    //   .x(function(d) {
+    //     return x(new Date(d.dateTime * 1000));
+    //   })
+    //   .y0(canvas.x)
+    //   .y1(function(d) {
+    //     return y(d.temp);
+    //   });
 
-    var lineGraph = this.state.canvas.node.append('g');
+    // const lineGraph = canvas.node.append('g');
 
-    var location = this.state.canvas.node
+    const location = canvas.node
       .append('g')
       .selectAll('.location')
-      .data(this.state.weatherData.apiResults.results)
+      .data(weatherData.apiResults.results)
       .enter()
       .append('g')
       .attr('class', 'location');
@@ -218,70 +189,60 @@ class D3Weather extends Component {
     location
       .append('path')
       .attr('class', 'line')
-      .attr('d', function(d) {
-        return line(d.forecast);
-      })
-      .style('stroke', function(d) {
-        return z(d.name);
-      })
+      .attr('d', (d) => line(d.forecast))
+      .style('stroke', (d) => z(d.name))
       .attr('fill', 'none');
-    this.state.canvas.node
+    canvas.node
       .append('g')
       .attr('class', 'axis axis--x')
-      .attr('transform', 'translate(0,' + this.state.canvas.y + ')')
+      .attr('transform', `translate(0, ${canvas.y})`)
       .call(
-        d3Axis.axisBottom(x).tickFormat(function(d) {
-          var formatMillisecond = d3TimeFormat.timeFormat('.%L'),
-            formatSecond = d3TimeFormat.timeFormat(':%S'),
-            formatMinute = d3TimeFormat.timeFormat('%H:%M'),
-            formatHour = d3TimeFormat.timeFormat('%H:00'),
-            formatDay = d3TimeFormat.timeFormat('%a %d'),
-            formatWeek = d3TimeFormat.timeFormat('%b %d'),
-            formatMonth = d3TimeFormat.timeFormat('%B'),
-            formatYear = d3TimeFormat.timeFormat('%Y'),
-            multiFormat = function(date) {
-              return (d3Time.timeSecond(date) < date
-                ? formatMillisecond
-                : d3Time.timeMinute(date) < date
-                  ? formatSecond
-                  : d3Time.timeHour(date) < date
-                    ? formatMinute
-                    : d3Time.timeDay(date) < date
-                      ? formatHour
-                      : d3Time.timeMonth(date) < date
-                        ? d3Time.timeWeek(date) < date
-                          ? formatDay
-                          : formatWeek
-                        : d3Time.timeYear(date) < date
-                          ? formatMonth
-                          : formatYear)(date);
-            };
+        d3Axis.axisBottom(x).tickFormat((d) => {
+          const formatMillisecond = d3TimeFormat.timeFormat('.%L');
+          const formatSecond = d3TimeFormat.timeFormat(':%S');
+          const formatMinute = d3TimeFormat.timeFormat('%H:%M');
+          const formatHour = d3TimeFormat.timeFormat('%H:00');
+          const formatDay = d3TimeFormat.timeFormat('%a %d');
+          const formatWeek = d3TimeFormat.timeFormat('%b %d');
+          const formatMonth = d3TimeFormat.timeFormat('%B');
+          const formatYear = d3TimeFormat.timeFormat('%Y');
+          const multiFormat = (date) => {
+            return (d3Time.timeSecond(date) < date
+              ? formatMillisecond
+              : d3Time.timeMinute(date) < date
+                ? formatSecond
+                : d3Time.timeHour(date) < date
+                  ? formatMinute
+                  : d3Time.timeDay(date) < date
+                    ? formatHour
+                    : d3Time.timeMonth(date) < date
+                      ? d3Time.timeWeek(date) < date
+                        ? formatDay
+                        : formatWeek
+                      : d3Time.timeYear(date) < date
+                        ? formatMonth
+                        : formatYear)(date);
+          };
           return multiFormat(d);
         }),
       );
 
-    this.state.canvas.node
+    canvas.node
       .append('g')
       .attr('class', 'axis y-axis')
       .call(
         d3Axis
           .axisLeft(y)
           .ticks(
-            Math.min(
-              Math.round(Math.floor(this.state.canvas.y / 35) + 1),
-              highestTemp,
-            ),
+            Math.min(Math.round(Math.floor(canvas.y / 35) + 1), highestTemp),
             '.0f',
           ),
       )
       .append('text')
       .attr(
         'transform',
-        'rotate(-90) translate(' +
-          -(this.state.canvas.y / 2) +
-          ', ' +
-          -this.state.canvas.margin.left * 0.8 +
-          ')',
+        `rotate(-90) translate(${-(canvas.y / 2)}, ${-canvas.margin.left *
+          0.8})`,
       )
       .attr('class', 'label')
       .attr('text-anchor', 'middle')
@@ -291,17 +252,18 @@ class D3Weather extends Component {
       .attr('dy', '.35em')
       .attr('fill', '#666')
       .text('Temp °C');
-    this.state.canvas.node.selectAll('.y-axis g text').attr('fill', '#666');
-    this.state.canvas.node.selectAll('.y-axis g line').attr('stroke', '#666');
+    canvas.node.selectAll('.y-axis g text').attr('fill', '#666');
+    canvas.node.selectAll('.y-axis g line').attr('stroke', '#666');
   };
 
   render() {
+    const { select } = this.state;
     return (
       <Container>
         <div className="App">
           <RadioButtons
             handleChange={this.handleSelectChange}
-            parentState={this.state.select}
+            parentState={select}
           />
           <div className="header">
             <h3 className="text-muted">D3 Implementations</h3>
