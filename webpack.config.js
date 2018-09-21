20e82f71c8622752e53f97261b5656c85bca7cad
John Williams <john@hothouse.co.nz>
2018-09-21 12:10:47 +1200

    edit gitignore add npmignore

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const htmlWebpackPlugin = new HtmlWebpackPlugin({
+  template: path.join(__dirname, 'examples/src/index.html'),
+  filename: './index.html',
+});
+module.exports = {
+  entry: path.join(__dirname, 'examples/src/index.js'),
+  output: {
+    path: path.join(__dirname, 'examples/dist'),
+    filename: 'index.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [htmlWebpackPlugin],
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  devServer: {
+    port: 3001,
+  },
+};
